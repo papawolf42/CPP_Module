159ebbafb0833a9c1d1ae6dcdbee73974024b666
papawolf42 <papawolf@kakao.com>
2022-01-27 04:57:02 +0900

    22.01.27 Add newline at end of file

--- Module_01/ex05/main.cpp
+++ Module_01/ex05/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gunkim <gunkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/27 00:31:42 by gunkim            #+#    #+#             */
-/*   Updated: 2022/01/27 02:57:09 by gunkim           ###   ########.fr       */
+/*   Updated: 2022/01/27 04:55:18 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,4 @@ int main() {
 	karen.complain("INFO");
 	karen.complain("WARNING");
 	karen.complain("ERROR");
-	karen.complain("aa");
 }
