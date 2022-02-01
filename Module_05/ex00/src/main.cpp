73011ac767cdb818e4e715d6bc029b40231d3346
papawolf42 <papawolf@kakao.com>
2022-02-01 17:50:18 +0900

    22.02.01 Make main uncomment

--- Module_05/ex00/src/main.cpp
+++ Module_05/ex00/src/main.cpp
@@ -6,52 +6,52 @@
 /*   By: gunkim <gunkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/29 06:48:39 by gunkim            #+#    #+#             */
-/*   Updated: 2022/01/31 13:49:49 by gunkim           ###   ########.fr       */
+/*   Updated: 2022/02/01 17:53:17 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 int main() {
-	// try {
-	// 	Bureaucrat president("Moon", 0);
-	// } catch (const std::exception& e) {
-	// 	std::cerr << e.what() << std::endl;
-	// }
-	// try {
-	// 	Bureaucrat president("Moon", 1);
-	// } catch (const std::exception& e) {
-	// 	std::cerr << e.what() << std::endl;
-	// }
-	// try {
-	// 	Bureaucrat president("Moon", 150);
-	// } catch (const std::exception& e) {
-	// 	std::cerr << e.what() << std::endl;
-	// }
-	// try {
-	// 	Bureaucrat president("Moon", 151);
-	// } catch (const std::exception& e) {
-	// 	std::cerr << e.what() << std::endl;
-	// }
-	// try {
-	// 	Bureaucrat president("Moon", 1);
-	// 	president.upGrade(42);
-	// } catch (const std::exception& e) {
-	// 	std::cerr << e.what() << std::endl;
-	// }
-	// try {
-	// 	Bureaucrat president("Moon", 1);
-	// 	president.downGrade(42);
-	// 	std::cout << president << std::endl;
-	// 	president.downGrade(42);
-	// 	std::cout << president << std::endl;
-	// 	president.downGrade(42);
-	// 	std::cout << president << std::endl;
-	// 	president.downGrade(42);
-	// 	std::cout << president << std::endl;
-	// } catch (const std::exception& e) {
-	// 	std::cerr << e.what() << std::endl;
-	// }
+	try {
+		Bureaucrat president("Moon", 0);
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+	try {
+		Bureaucrat president("Moon", 1);
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+	try {
+		Bureaucrat president("Moon", 150);
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+	try {
+		Bureaucrat president("Moon", 151);
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+	try {
+		Bureaucrat president("Moon", 1);
+		president.upGrade(42);
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+	try {
+		Bureaucrat president("Moon", 1);
+		president.downGrade(42);
+		std::cout << president << std::endl;
+		president.downGrade(42);
+		std::cout << president << std::endl;
+		president.downGrade(42);
+		std::cout << president << std::endl;
+		president.downGrade(42);
+		std::cout << president << std::endl;
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
 
 	// } catch (Bureaucrat::GradeTooHighException& h) {
 	// 	std::cerr << h.what() << std::endl;
