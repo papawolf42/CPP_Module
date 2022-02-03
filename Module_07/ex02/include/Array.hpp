553687b8e037a34548e3dc73d1cb11cabdd0083c
papawolf42 <papawolf@kakao.com>
2022-02-03 13:33:14 +0900

    22.02.03 Submit Module07 ex02

--- Module_07/ex02/include/Array.hpp
+++ Module_07/ex02/include/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: gunkim <gunkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 12:11:35 by gunkim            #+#    #+#             */
-/*   Updated: 2022/02/03 12:39:22 by gunkim           ###   ########.fr       */
+/*   Updated: 2022/02/03 13:20:45 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,10 @@
 
 # include <exception>
 
+template <typename T>
 class Array {
 	private:
-		int				*array_;
+		T				*array_;
 		unsigned int	size_;
 
 	public:
@@ -27,11 +28,11 @@ class Array {
 		}
 		Array(unsigned int n) {
 			size_ = n;
-			array_ = new int[size_];
+			array_ = new T[size_];
 		}
 		Array(const Array& ref) {
 			size_ = ref.size_;
-			array_ = new int[size_];
+			array_ = new T[size_];
 			for (unsigned int i = 0; i < size_; i++) {
 				array_[i] = ref.array_[i];
 			}
@@ -43,13 +44,13 @@ class Array {
 		Array& operator=(const Array& ref) {
 			delete[] array_;
 			size_ = ref.size_;
-			array_ = new int[size_];
+			array_ = new T[size_];
 			for (unsigned int i = 0; i < size_; i++) {
 				array_[i] = ref.array_[i];
 			}
 			return (*this);
 		}
-		int& operator[](unsigned int i) {
+		T& operator[](unsigned int i) {
 			if (i < 0 || size_ <= i)
 				throw outBoundException();
 			return (array_[i]);
@@ -66,5 +67,56 @@ class Array {
 			}
 		};
 };
+// class Array {
+// 	private:
+// 		int				*array_;
+// 		unsigned int	size_;
+
+// 	public:
+// 		Array() {
+// 			size_ = 0;
+// 			array_ = NULL;
+// 		}
+// 		Array(unsigned int n) {
+// 			size_ = n;
+// 			array_ = new int[size_];
+// 		}
+// 		Array(const Array& ref) {
+// 			size_ = ref.size_;
+// 			array_ = new int[size_];
+// 			for (unsigned int i = 0; i < size_; i++) {
+// 				array_[i] = ref.array_[i];
+// 			}
+// 		}
+// 		~Array() {
+// 			delete[] array_;
+// 		}
+
+// 		Array& operator=(const Array& ref) {
+// 			delete[] array_;
+// 			size_ = ref.size_;
+// 			array_ = new int[size_];
+// 			for (unsigned int i = 0; i < size_; i++) {
+// 				array_[i] = ref.array_[i];
+// 			}
+// 			return (*this);
+// 		}
+// 		int& operator[](unsigned int i) {
+// 			if (i < 0 || size_ <= i)
+// 				throw outBoundException();
+// 			return (array_[i]);
+// 		}
+
+// 		unsigned int size() {
+// 			return (size_);
+// 		}
+
+// 		class outBoundException : public std::exception {
+// 			public:
+// 			const char* what() const throw() {
+// 				return ("cannot access, index is out of the limits");
+// 			}
+// 		};
+// };
 
 #endif
--- Module_07/ex02/src/main.cpp
+++ Module_07/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gunkim <gunkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 12:11:32 by gunkim            #+#    #+#             */
-/*   Updated: 2022/02/03 12:39:04 by gunkim           ###   ########.fr       */
+/*   Updated: 2022/02/03 13:32:28 by gunkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 #include "Array.hpp"
 
 int main() {
+	/*
+	** first test for copy constructor, int array
+	*/
 	try {
-		Array arr_int(6);
+		Array<int> arr_int(6);
 		
 		arr_int[0] = 4;
 		arr_int[1] = 8;
@@ -25,30 +28,84 @@ int main() {
 		arr_int[5] = 42;
 		// arr_int[6] = 999;
 
+		std::cout << "arr_int : ";
 		for(unsigned int i = 0; i < arr_int.size(); i++) {
 			std::cout << arr_int[i] << " ";
 		}
 		std::cout << std::endl;
 
-		Array temp(arr_int);
+		Array<int> temp(arr_int);
+		std::cout << "temp    : ";
 		for(unsigned int i = 0; i < temp.size(); i++) {
 			std::cout << temp[i] << " ";
 		}
 		std::cout << std::endl;
 
 		temp[5] = 24;
+		std::cout << "arr_int : ";
 		for(unsigned int i = 0; i < arr_int.size(); i++) {
 			std::cout << arr_int[i] << " ";
 		}
 		std::cout << std::endl;
+		std::cout << "temp    : ";
 		for(unsigned int i = 0; i < temp.size(); i++) {
 			std::cout << temp[i] << " ";
 		}
 		std::cout << std::endl;
 
-	} catch (Array::outBoundException& e) {
+	// } catch (Array::outBoundException& e) {
+	} catch (std::exception& e) {
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+
+	/*
+	** second test for assignation operator, string array
+	*/
+	try {
+		Array<std::string> arr_str(3);
+		arr_str[0] = "life";
+		arr_str[1] = "is";
+		arr_str[2] = "42";
+
+		std::cout << "arr_str : ";
+		for(unsigned int i = 0; i < arr_str.size(); i++) {
+			std::cout << arr_str[i] << " ";
+		}
+		std::cout << std::endl;
+
+		Array<std::string> temp;
+		std::cout << "temp    : ";
+		for(unsigned int i = 0; i < temp.size(); i++) {
+			std::cout << temp[i] << " ";
+		}
+		std::cout << std::endl;
+
+		// assgination occurs
+		std::cout << "assgination occurs" << std::endl;
+		temp = arr_str;
+		std::cout << "temp    : ";
+		for(unsigned int i = 0; i < temp.size(); i++) {
+			std::cout << temp[i] << " ";
+		}
+		std::cout << std::endl;
+
+		// [] operator works
+		std::cout << "[]operator works" << std::endl;
+		temp[2] = "*";
+		std::cout << "temp    : ";
+		for(unsigned int i = 0; i < temp.size(); i++) {
+			std::cout << temp[i] << " ";
+		}
+		std::cout << std::endl;
+		std::cout << "arr_str : ";
+		for(unsigned int i = 0; i < arr_str.size(); i++) {
+			std::cout << arr_str[i] << " ";
+		}
+		std::cout << std::endl;
+	} catch (std::exception& e) {
 		std::cerr << e.what() << std::endl;
 	}
 
-	system("leaks Array");
+	// system("leaks Array");
 }
